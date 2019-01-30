f103ae946ba2e53becdb428ec684e1e937a59868
David Corne <david.corne@autodesk.com>
2019-01-30 09:45:15 +0000

    Generate a sudoku and distringuish between true and display values.

--- src/index.js
+++ src/index.js
@@ -4,27 +4,95 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class gameSquare {
-    constructor(grid) {
-        this.grid = grid;
-        this.value = null;
-        this.trueValue = null;
-    }
-}
-
-class gameGrid {
-    constructor(board) {
+    constructor(board, x, y) {
         this.board = board;
-        this.array = Array(9).fill(new gameSquare(this));
+        this.displayValue = null;
+        this.trueValue = null;
+        this.guesses = [];
+        this.x = x;
+        this.y = y;
     }
 }
 
 class gameBoard {
     constructor() {
-        this.array = Array(9).fill(new gameGrid(this));
+        this.array = [];
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 9; j++) {
+                this.array.push(new gameSquare(this, i, j));
+            }
+        }
+        this.generate();
+    }
+
+    setSudoku(trueValues, displayValues) {
+        if (trueValues.length !== 81) throw Error('Wrong trueValues array length');
+        if (displayValues.length !== 81) throw Error('Wrong displayValues array length');
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 9; j++) {
+                this.square(i, j).trueValue = trueValues[i * 9 + j];
+                this.square(i, j).displayValue = displayValues[i * 9 + j];
+            }
+        }
+
+    }
+    generate() {
+        // Should generate a sudoku, hard-code one for now.
+        // 8 4 5 | 6 3 2 | 1 7 9 
+        // 7 3 2 | 9 1 8 | 6 5 4 
+        // 1 9 6 | 7 4 5 | 3 2 8 
+        // ---------------------
+        // 6 8 3 | 5 7 4 | 9 1 2
+        // 4 5 7 | 2 9 1 | 8 3 6
+        // 2 1 9 | 8 6 3 | 5 4 7
+        // ---------------------
+        // 3 6 1 | 4 2 9 | 7 8 5
+        // 5 7 4 | 1 8 6 | 2 9 3
+        // 9 2 8 | 3 5 7 | 4 6 1
+        const trueValues = [
+            8, 4, 5, 6, 3, 2, 1, 7, 9,
+            7, 3, 2, 9, 1, 8, 6, 5, 4, 
+            1, 9, 6, 7, 4, 5, 3, 2, 8, 
+            6, 8, 3, 5, 7, 4, 9, 1, 2,
+            4, 5, 7, 2, 9, 1, 8, 3, 6,
+            2, 1, 9, 8, 6, 3, 5, 4, 7,
+            3, 6, 1, 4, 2, 9, 7, 8, 5,
+            5, 7, 4, 1, 8, 6, 2, 9, 3,
+            9, 2, 8, 3, 5, 7, 4, 6, 1,
+        ]
+        // 0 4 0 | 0 0 0 | 1 7 9 
+        // 0 0 2 | 0 0 8 | 0 5 4 
+        // 0 0 6 | 0 0 5 | 0 0 8 
+        // ---------------------
+        // 0 8 0 | 0 7 0 | 9 1 0 
+        // 0 5 0 | 0 9 0 | 0 3 0 
+        // 0 1 9 | 0 6 0 | 0 4 0 
+        // ---------------------
+        // 3 0 0 | 4 0 0 | 7 0 0 
+        // 5 7 0 | 1 0 0 | 2 0 0 
+        // 9 2 8 | 0 0 0 | 0 6 0 
+
+        const displayValues = [
+            0, 4, 0, 0, 0, 0, 1, 7, 9, 
+            0, 0, 2, 0, 0, 8, 0, 5, 4, 
+            0, 0, 6, 0, 0, 5, 0, 0, 8, 
+            0, 8, 0, 0, 7, 0, 9, 1, 0, 
+            0, 5, 0, 0, 9, 0, 0, 3, 0, 
+            0, 1, 9, 0, 6, 0, 0, 4, 0, 
+            3, 0, 0, 4, 0, 0, 7, 0, 0, 
+            5, 7, 0, 1, 0, 0, 2, 0, 0, 
+            9, 2, 8, 0, 0, 0, 0, 6, 0, 
+        ]
+        this.setSudoku(trueValues, displayValues);
+    }
+
+    square(x, y) {
+        if (0 > x || x > 8) throw Error('Wrong x coordinate: ' + x);
+        if (0 > y || y > 8) throw Error('Wrong y coordinate: ' + y);
+        return this.array[x * 9 + y];
     }
     
 }
-
 
 class Square extends React.Component {
     constructor(props) {
@@ -35,10 +103,9 @@ class Square extends React.Component {
         }
     }
     render() {
-        console.log(this.props)
       return (
         <button className="square">
-          {this.props.x},{this.props.y}
+          {this.props.square.displayValue}
         </button>
       );
     }
@@ -52,7 +119,7 @@ class Board extends React.Component {
         }
     }
     renderSquare(x, y) {
-        return (<Square x={x} y={y}/>);
+        return (<Square square={this.state.gameBoard.square(x, y)}/>);
       }
   
     createGrid() {
